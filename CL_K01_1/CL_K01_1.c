d0aa4bf050fe7dea94b089710a0f34b8ad112744
utidataihei <bctu1220047@gn.iwasaki.ac.jp>
2013-01-09 10:27:25 +0900

    応用プログラミングA の課題1

---
+++ CL_K01_1/CL_K01_1.c
@@ -0,0 +1,14 @@
+/*ì¬Ò:1T3(4) “à“c‘×•½
+‰Û‘è–¼:‰Û‘è01_1*/
+
+
+#include <stdio.h>
+
+void main(void)
+{
+	int width,sum;
+	width=5;
+	sum=width*width;
+	
+		printf("ˆê•Ó‚Ì’·‚³5‚Ì³•ûŒ`‚Ì–ÊÏ‚Í%d‚Å‚·B\n",sum);
+}
